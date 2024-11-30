a765b5cabb8241ba5eeab89ba07dacde91706783
Tahar BANOUN <tbanoun@wissalgroup.com>
2024-11-30 18:22:34 +0100

    creer la logique pour la creation d'une oportunity

--- modles/common.py
+++ modles/common.py
@@ -1,3 +1,5 @@
+from odoo import _
+
 def checkUserHasGroup(self, group):
     """"
      Vérifie si l'utilisateur actuel appartient à un groupe spécifique dans Odoo
@@ -5,3 +7,18 @@ def checkUserHasGroup(self, group):
     user = self.env.user
     if user.has_group(group): return True
     return False
+
+
+def show_notification(status,title, message):
+    """"""
+    notification = {
+        'type': 'ir.actions.client',
+        'tag': 'display_notification',
+        'params': {
+            'title': _(f'{title}'),
+            'type': f'{status}',
+            'message': f"{message}",
+            'sticky': False,
+        }
+    }
+    return notification
--- modles/res_partner.py
+++ modles/res_partner.py
@@ -1,5 +1,7 @@
 from odoo import api, models, fields
+from odoo.tools.safe_eval import datetime
 from .common import *
+from datetime import datetime
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
@@ -8,6 +10,13 @@ class ResPartner(models.Model):
         action = self.env['ir.actions.act_window']._for_xml_id(
             'dnd_security.action_contacts_request_cration')
         action["res_id"] = res_id
+        return action
+
+    def renderCrmLeadFormPage(self, res_id):
+        action = self.env['ir.actions.act_window']._for_xml_id(
+            'crm.crm_lead_action_pipeline')
+        action["res_id"] = res_id
+        action["view_mode"] = 'form'
         return action
 
 
@@ -21,9 +30,23 @@ class ResPartner(models.Model):
 
 
     def action_confirm(self):
-        print("Confirm ok!")
-        pass
+        self.active = True
+        # créer une opportunité
+        year = datetime.now().year
+        vals = {
+            "name": "New Lead %s" % self.display_name,
+            "partner_id": self.id,
+            "type": "opportunity",
+            "year": year,
+            'user_id': self.create_uid.id
+        }
+        res = self.env['crm.lead'].sudo().create(
+            vals
+        )
+        action = self.renderCrmLeadFormPage(res.id)
+        return action
 
     def action_cancel(self):
-        print("Delete OK!")
-        pass
+        notification = show_notification('success', 'Information', f'Le contact {self.display_name} a été supprimé avec succès.')
+        self.sudo().unlink()
+        return notification
